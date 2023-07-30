9049f07453ee170a321cfcf951059dd216b8361a
Malek I Rhaouate <malek@Maleks-MacBook-Pro.local>
2023-07-30 14:06:05 -0400

    Added contact and payments functionality to the app

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-
+import secrets
 from authentication.auth_tools import login_pipeline, update_passwords, hash_password
 from database.db import Database
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from core.session import Sessions
 
 app = Flask(__name__)
+# Generate a secret key for the session
+secret_key = secrets.token_hex(16)
+app.secret_key = secret_key
+
 HOST, PORT = 'localhost', 8080
 global username, products, db, sessions
 username = 'default'
@@ -26,6 +30,7 @@ def index_page():
     returns:
         - None
     """
+    logged_in = 'logged_in' in session and session['logged_in']
     return render_template('index.html', username=username, products=products, sessions=sessions)
 
 
@@ -56,17 +61,19 @@ def login():
 
     modifies:
         - sessions: adds a new session to the sessions object
-
+        - session: sets the 'logged_in' variable to True upon successful login
     """
+    global username
     username = request.form['username']
     password = request.form['password']
     if login_pipeline(username, password):
         sessions.add_new_session(username, db)
+        # Set 'logged_in' session variable to True upon successful login
+        session['logged_in'] = True
         return render_template('home.html', products=products, sessions=sessions)
     else:
         print(f"Incorrect username ({username}) or password ({password}).")
-        return render_template('index.html')
-
+        return render_template('index.html', logged_in=False)  # Pass logged_in status here
 
 @app.route('/register')
 def register_page():
@@ -124,17 +131,74 @@ def checkout():
     """
     order = {}
     user_session = sessions.get_session(username)
+
+    # Add price information to the order dictionary
     for item in products:
-        print(f"item ID: {item['id']}")
-        if request.form[str(item['id'])] > '0':
-            count = request.form[str(item['id'])]
-            order[item['item_name']] = count
-            user_session.add_new_item(
-                item['id'], item['item_name'], item['price'], count)
+        item_id = str(item['id'])
+        if request.form[item_id] > '0':
+            count = int(request.form[item_id])
+            item_name = item['item_name']
+            price_per_item = item['price']
+            order[item_name] = {
+                'quantity': count,
+                'price_per_item': price_per_item,
+                'total_price': price_per_item * count
+            }
+
+            user_session.add_new_item(item_id, item_name, price_per_item, count)
 
     user_session.submit_cart()
 
-    return render_template('checkout.html', order=order, sessions=sessions, total_cost=user_session.total_cost)
+    return render_template('checkout.html', order=order, total_cost=user_session.total_cost)
+
+@app.route('/payment', methods=['GET'])
+def payment():
+    # Retrieve the user's session
+    user_session = sessions.get_session(username)
+
+    if user_session is None:
+        return redirect(url_for('login_page'))
+
+    return render_template('payment.html', total_cost=user_session.total_cost)
+
+@app.route('/process_payment', methods=['POST'])
+def process_payment():
+
+    # In a real application, you would handle the payment processing here.
+    # For simplicity, we'll just redirect the user back to the home page after processing.
+    
+    
+    # For now, let's assume the payment was successful:
+    user_session = sessions.get_session(username)
+    if user_session is not None:
+        user_session.clear_cart()  # Clear the user's cart after successful payment
+
+    return redirect(url_for('index_page'))
+
+# Search for a product
+@app.route('/search', methods=['GET'])
+def search_products():
+    search_query = request.args.get('q', '')  # Get the search query from the URL parameters
+    filtered_products = [product for product in products if search_query.lower() in product['item_name'].lower()]
+
+    return render_template('home.html', products=filtered_products, sessions=sessions)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    # Check if the user is logged in
+    logged_in = 'logged_in' in session and session['logged_in']
+
+    if request.method == 'POST':
+        # Get the data from the submitted form
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        return redirect(url_for('contact'))
+
+    return render_template('contact.html', logged_in=logged_in)  # Pass the logged_in variable to the template
+
+
 
 
 if __name__ == '__main__':
