b7a84deefb2e02839cb1297efb7b01fdb295550c
Saamyar Alizadeh <saamyar@Saamyars-Air.attlocal.net>
2023-08-08 18:26:45 -0400

    Updated final product

--- database/reset_database.py
+++ database/reset_database.py
@@ -33,15 +33,13 @@ def execute_sql_file(cursor: sqlite3.Cursor, sql_file_path: str) -> None:
         cursor.executescript(sql)
     return None
 
-def create_database(schema_path: str, starting_data_path: str, starting_data_reviews_path: str, starting_data_rewards_path: str, database_path: str) -> None:
+def create_database(schema_path: str, starting_data_path: str, database_path: str) -> None:
     """
     Set the database schema.
     
     args:
         - schema_path: The path to the schema file.
         - starting_data_path: The path to the starting data file.
-        - starting_data_reviews_path: The path to the starting data reviews file.
-        - starting_data_rewards_path: The path to the starting data rewards file.
         - database_path: The path to the database file.
 
     returns:
@@ -55,8 +53,6 @@ def create_database(schema_path: str, starting_data_path: str, starting_data_rev
     # Builds the database from the schema and starting data.
     execute_sql_file(cursor, schema_path)
     execute_sql_file(cursor, starting_data_path)
-    execute_sql_file(cursor, starting_data_reviews_path) 
-    execute_sql_file(cursor, starting_data_rewards_path)  
 
     # Commits the changes and closes the connection.
     connection.commit()
@@ -69,12 +65,11 @@ def main() -> None:
     database_path = os.path.join(os.path.dirname(__file__), 'store_records.db')
     schema_path = os.path.join(os.path.dirname(__file__), 'schema.sql')
     starting_data_path = os.path.join(os.path.dirname(__file__), 'starting_data.sql')
-    starting_data_reviews_path = os.path.join(os.path.dirname(__file__), 'starting_data_reviews.sql') 
-    starting_data_rewards_path = os.path.join(os.path.dirname(__file__), 'starting_data_rewards.sql')  
+    
 
     # Removes the database file and creates a new one.
     remove_database(database_path)
-    create_database(schema_path, starting_data_path, starting_data_reviews_path, starting_data_rewards_path, database_path)
+    create_database(schema_path, starting_data_path, database_path)
     return None
 
 if __name__ == '__main__':
--- database/starting_data.sql
+++ database/starting_data.sql
@@ -10,22 +10,22 @@ VALUES ('llamport', '9171d14954eeda4e70777c23d98e349818125cdaeb884ff97ebf8cc0a9c
 INSERT into `users` (`username`, `password_hash`, `email`, `first_name`, `last_name`)
 VALUES ('bliskov', '1e4b9ae956cad1385cfa6fffd8323dd16c3fe18c54e6447e49bddef2138d042e84e1505a541c6ef19a5026e684b2559efd366145870a0a8d4d4173c0877f6cd2', 'barbara@thor.com', 'Barbara', 'Liskov');
 
-INSERT INTO inventory (item_name, info, price, stock, image_url, category) 
+INSERT INTO `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`) 
 VALUES ('FCB', 'FC Barcelona Jersey', 50.00, 100, 'static/images/fcb_jersey.jpeg', 'Sport');
 
-INSERT INTO inventory (item_name, info, price, stock, image_url, category) 
+INSERT INTO `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`) 
 VALUES ('ACM', 'AC Milan Jersey', 50.00, 100, 'static/images/acm_jersey.jpeg', 'Sport');
 
-INSERT INTO inventory (item_name, info, price, stock, image_url, category) 
+INSERT INTO `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)
 VALUES ('RMA', 'Real Madrid Jersey', 50.00, 100, 'static/images/rma_jersey.jpeg', 'Sport');
 
-INSERT INTO inventory (item_name, info, price, stock, image_url, category) 
+INSERT INTO `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`) 
 VALUES ('ARS', 'Arsenal Jersey', 50.00, 100, 'static/images/ars_jersey.jpeg', 'Sport');
 
-INSERT INTO inventory (item_name, info, price, stock, image_url, category) 
+INSERT INTO `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`) 
 VALUES ('Cleats', 'Nike Cleats', 100.00, 100, 'static/images/boot.jpeg', 'Sport');
 
-INSERT INTO inventory (item_name, info, price, stock, image_url, category) 
+INSERT INTO `inventory` (`item_name`, `info`, `price`, `stock`, `image_url`, `category`)  
 VALUES ('Shin Guards', 'Nike Shin Guards', 50.00, 100, 'static/images/nike_guards.jpeg', 'Sport');
 
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
@@ -37,10 +37,11 @@ VALUES ('2', 'dritchie', '2', 10, '2022-12-21 7:30:30', 5.50);
 INSERT into `sales` (`transaction_id`, `username`, `item_id`, `quantity`, `sale_date`, `cost`)
 VALUES ('3', 'llamport', '3', 10, '2022-12-21 7:30:30', 5.50);
 
-INSERT INTO product_reviews (item_id, username, review_text, review_date)
-VALUES (1, 'user1', 'This product is amazing!', '2023-07-30 12:00:00'),
+INSERT INTO `product_reviews` (`item_id`, `username`, `review_text`, `review_date`)
+VALUES (1, 'user1', 'This product is amazing!', '2023-07-30 12:00:00');
 
 
-INSERT INTO rewards (reward_name, description, points_required, image_url)
-VALUES ('10% Discount Coupon', 'Get a 10% discount on your next purchase.', 100, 'static/images/nike_guards.jpeg'),
-       ('Free Shipping', 'Redeem this reward for free shipping on your order.', 150, 'static/images/nike_guards.jpeg');
+INSERT INTO `rewards` (`reward_name`, `description`, `points_required`, `image_url`)
+VALUES ('10% Discount Coupon', 'Get a 10% discount on your next purchase.', 100, 'static/images/nike_guards.jpeg');
+INSERT INTO `rewards` (`reward_name`, `description`, `points_required`, `image_url`)
+Values ('Free Shipping', 'Redeem this reward for free shipping on your order.', 150, 'static/images/nike_guards.jpeg');
